a5c17c76353f4c179687a6ba102e82143e75548b
justneil11 <justneil11@gmail.com>
2023-05-19 17:47:23 +0100

    Created container and first components

---
+++ client/src/Containers/MainContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import '../App.css';
+import logo from '../logo.svg';
+
+const MainContainer = () => {
+
+return(
+    <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            How Selfish Is Your Trip? : Coming Soon™ 
+        </p>
+        </header>
+    </div>
+
+    // <Router>
+    //     <Header "add properties"/>
+    //     <Routes>
+    //         <Route path "/" element = { <Dashboard "add properties"/>} />
+    //         <Route path "/create_trip" element = { <TripForm "add properties"/>} />
+    //         <Route path "my_trips" element = { <TripsList "add properties"/> }/>
+    //     </Routes>
+    // </Router>
+)
+
+}
+
+export default MainContainer
