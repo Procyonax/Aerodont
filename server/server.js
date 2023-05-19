fe7edd4b26efea473067ccfd5a8e19bc86fa9fba
Procyonax <deoxtw@gmail.com>
2023-05-19 11:43:23 +0100

    Added server router

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const createRouter = require('./helpers/create_router.js');
 
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
 
-
 app.listen(9000, function() {
   console.log(`Trips server running on port ${this.address().port}`);
 });
