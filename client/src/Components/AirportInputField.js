0278bafe032acb1a8d8aa8941de92b7578e4d77c
Zoe Eustace <zoeeustace@zoes-air.home>
2023-05-21 16:06:08 +0100

    Removed axios

--- client/src/Components/AirportInputField.js
+++ client/src/Components/AirportInputField.js
@@ -8,17 +8,24 @@ const AirportInputField = () => {
     const [suggestions, setSuggestions] = useState([])
 
     useEffect(() => {
-        const loadAirports = async() => {
-            const response = await axios.get('http://localhost:3000/')
-            .then((data) => setAirports(data.name))
-        }
         loadAirports()
     }, [])
 
+    const loadAirports =  () => {
+        const airportFetches = []
+        for(let airport of airports){
+            console.log(airport);
+            const newFetch = fetch('http://localhost:3000/')
+            .then(response => response.json())
+            airportFetches.push(newFetch)
+        }
+        Promise.all(airportFetches)
+        .then(data => setAirports(data))
+    }
 
     const onChangeHandler = (text) => {
         let matches = []
-        console.log(airports);
+        // console.log(airports);
         if(text.length>0){
             matches = airports.filter(airport => {
                 const regex = new RegExp(`${text}`, "gi");
