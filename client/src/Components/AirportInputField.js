4ef1c87f4887e7642abdde0350badcb47df55000
Zoe Eustace <zoeeustace@zoes-air.home>
2023-05-21 21:17:03 +0100

    front end of input suggestion complete

--- client/src/Components/AirportInputField.js
+++ client/src/Components/AirportInputField.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import Papa from 'papaparse'
 
 const AirportInputField = () => {
 
@@ -15,36 +15,59 @@ const AirportInputField = () => {
         loadAirports()
     }, [])
 
-    const loadAirports =  () => {
-        const airportFetches = []
-        for(let airport of airports){
-            const newFetch = fetch('http://localhost:3000/')
-            airportFetches.push(newFetch)
-        }
-        Promise.all(airportFetches)
-        .then(data => setAirports(data))
-    }
+    const loadAirports = () => {
+        fetch('https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv')
+          .then(response => {
+            return response.text();
+          })
+          .then(csv => {
+            const {data} = Papa.parse(csv, { header: true });
+            // console.log(data);
+            const finalData = data.filter(airport => airport["iata_code"] !== "" && airport["iata_code"] !=="0" && airport["iata_code"] !== "-")
+            // console.log(finalData);
+            finalData.pop()
+            setAirports(finalData);
+          })
+          .catch(error => {
+            console.error('Error occurred:', error);
+          });
+      };
+
 
     const onChangeHandler = (text) => {
         let matches = []
-        console.log(airports);
+        // console.log(airports);
         if(text.length>0){
             matches = airports.filter(airport => {
                 const regex = new RegExp(`${text}`, "gi");
                 return airport.name.match(regex)
             })
         }
-        console.log('matches', matches);
+        // console.log('matches', matches);
         setSuggestions(matches)
         setText(text)
     }
 
+    const onSuggestHandler = (text) => {
+        setText(text);
+        setSuggestions([]);
+    }
+
     return ( 
         <div>
-            <div>{text}</div>
+            {/* <div>{text}</div> */}
         <input type="text"
         onChange={event => onChangeHandler(event.target.value)}
-        value={text}/>
+        value={text}
+        onBlur={()=>{
+            setTimeout(()=>{
+                setSuggestions([])
+            },100);
+        }}/>
+        {suggestions && suggestions.map((suggestion, i)=>
+        <div key={i} onClick={() => onSuggestHandler(suggestion.name)}>
+            {suggestion.name}
+        </div>)}
         </div>
      );
 }
