2df4afd27ee0390801698bf1231f0bba867c82f5
Zoe Eustace <zoeeustace@zoes-air.home>
2023-05-21 15:53:39 +0100

    onChange added

--- client/src/Components/AirportInputField.js
+++ client/src/Components/AirportInputField.js
@@ -3,19 +3,40 @@ import axios from 'axios';
 
 const AirportInputField = () => {
 
-    const [airport, setAirport] = useState([])
+    const [airports, setAirports] = useState([])
+    const [text, setText] = useState('')
+    const [suggestions, setSuggestions] = useState([])
+
     useEffect(() => {
         const loadAirports = async() => {
-            const response = await axios.get('https://beta4.api.climatiq.io/travel/flights');
-            setAirport(response.legs.from.data)
+            const response = await axios.get('http://localhost:3000/')
+            .then((data) => setAirports(data.name))
         }
         loadAirports()
     }, [])
 
+
+    const onChangeHandler = (text) => {
+        let matches = []
+        console.log(airports);
+        if(text.length>0){
+            matches = airports.filter(airport => {
+                const regex = new RegExp(`${text}`, "gi");
+                return airport.name.match(regex)
+            })
+        }
+        console.log('matches', matches);
+        setSuggestions(matches)
+        setText(text)
+    }
+
     return ( 
-        <>
-        <input type="text"/>
-        </>
+        <div>
+            <div>{text}</div>
+        <input type="text"
+        onChange={event => onChangeHandler(event.target.value)}
+        value={text}/>
+        </div>
      );
 }
  
