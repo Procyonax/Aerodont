c3e4114ce203e4bd5dc8cc002570af28ed26e1d4
Cooper-Higgins <jamie@supplantree.com>
2023-05-21 08:44:08 +0100

    Dashboard populated and link to my_trips working

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useState} from "react";
+import { Navigate } from "react-router-dom"
 import '../App.css';
-import logo from '../logo.svg';
 
 const Dashboard = () => {
+    
+    const [viewTrips, setViewTrips] = useState(false);
+    
+    if (viewTrips) {
+        return <Navigate to="/my_trips" />;
+    }
+
     return(
-        <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                How Selfish Is Your Trip? : Coming Soon™ 
-            </p>
+        <div className="dashboard">
+            <header className="dash-header">
+                <h1>Dashboard</h1>
             </header>
+            <main className="dash-chart">
+                <div>Placeholder for chart plugin</div>
+                <div className="dash-results">
+                    <div>CO2 & num</div>
+                    <div>Trips & num</div>
+                    <div>Average & num</div>
+                </div>
+                <button onClick={() => {setViewTrips(true)}}>View Trips</button>
+            </main>
         </div> 
     )
 }
 
-export default Dashboard
+export default Dashboard;
