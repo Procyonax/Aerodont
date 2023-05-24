da5e1c3fdf8f3d5a1166a2f22f4da5352759bc03
Procyonax <deoxtw@gmail.com>
2023-05-24 12:01:28 +0100

    added the correct seeds file, thanks ZOE

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,19 +5,22 @@ db.trips.insertMany([
   {
     from: "London Heathrow Airport",
     to: "Berlin Brandenburg Airport",
+    cabin: "economy",
     nights: 0,
     footprint: 290.33337299472,
   },
   {
     from: "Glasgow International Airport",
     to: "Amsterdam Airport Schiphol",
-    nights: 0,
+    cabin: "economy",
+    nights: 14,
     footprint: 216.85157642736,
   },
   {
     from: "London Gatwick Airport",
     to: "John F Kennedy International Airport",
-    nights: 0,
+    cabin: "first",
+    nights: 7,
     footprint: 1646.9045494765198,
   },
 ]);
